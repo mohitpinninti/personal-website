91991f99be359c099542da89ae2a697134e66e0b
Mohit Pinninti <pinnintimohit@gmail.com>
2023-09-07 19:25:15 -0400

    links - take 1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../index.css'
 
 const NavBar = () => {
@@ -11,13 +12,16 @@ const NavBar = () => {
         <li>Blog</li>
         <li>Contact</li>
       </ul> */}
-      <h1 className='navname'>Mohit Pinninti</h1>
+      <h1><Link to={"/"} className='navname'>Mohit Pinninti</Link></h1>
       <ul>
-        {/* <li>Mohit Pinninti</li> */}
-        <li>Home</li>
+        {/* <li>Home</li>
         <li>About</li>
         <li>Blog</li>
-        <li>Contact</li>
+        <li>Contact</li> */}
+        <li><Link to={"/"} className='navlink'>Home</Link></li>
+        <li><Link to={"/about"} className='navlink'>About</Link></li>
+        <li><Link to={"/blog"} className='navlink'>Blog</Link></li>
+        <li><Link to={"/contact"} className='navlink'>Contact</Link></li>
       </ul>
     </nav>
   );
