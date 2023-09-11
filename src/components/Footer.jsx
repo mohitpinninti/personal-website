3239c828947fa740520b61639912fcf2eda1e451
Mohit Pinninti <pinnintimohit@gmail.com>
2023-09-11 00:14:32 -0400

    updated social media icon buttons with hover & active

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className="footer">
             <p>Designed in Figma, built with React.js, and deployed with Vercel.</p>
             <div className="footer-links">
-                <a href="https://github.com/mohitpinninti/"><FontAwesomeIcon icon={faGithub} size="2x" color="#aba2c7"/></a>
-                <a href="https://www.instagram.com/mohitpinninti/"><FontAwesomeIcon icon={faInstagram} size="2x" color="#aba2c7"/></a>
-                <a href="https://www.linkedin.com/in/mohit-pinninti/"><FontAwesomeIcon icon={faLinkedin} size="2x" color="#aba2c7"/></a>
+                <a href="https://github.com/mohitpinninti/"><FontAwesomeIcon className="footer-link" icon={faGithub} size="2x" color="#aba2c7"/></a>
+                <a href="https://www.instagram.com/mohitpinninti/"><FontAwesomeIcon className="footer-link" icon={faInstagram} size="2x" color="#aba2c7"/></a>
+                <a href="https://www.linkedin.com/in/mohit-pinninti/"><FontAwesomeIcon className="footer-link" icon={faLinkedin} size="2x" color="#aba2c7"/></a>
             </div>
         </div>
     );
