564fde39becbc37d8b73eae8fbbbf8beff504aec
Mohit Pinninti <pinnintimohit@gmail.com>
2023-09-23 14:05:13 -0400

    updated footer with email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
 
@@ -7,6 +9,7 @@ const Footer = () => {
         <div className="footer">
             <p>Designed in Figma, built with React.js, and deployed with Vercel.</p>
             <div className="footer-links">
+                <a href="mailto:pinnintimohit@gmail.com"><FontAwesomeIcon className="footer-link" icon={faEnvelope} size="2x" color="#aba2c7"/></a>
                 <a href="https://github.com/mohitpinninti/"><FontAwesomeIcon className="footer-link" icon={faGithub} size="2x" color="#aba2c7"/></a>
                 <a href="https://www.instagram.com/mohitpinninti/"><FontAwesomeIcon className="footer-link" icon={faInstagram} size="2x" color="#aba2c7"/></a>
                 <a href="https://www.linkedin.com/in/mohit-pinninti/"><FontAwesomeIcon className="footer-link" icon={faLinkedin} size="2x" color="#aba2c7"/></a>
