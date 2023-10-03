a6d126c01c112c14d2f2dd1c0efdf71ad886b125
Mohit Pinninti <pinnintimohit@gmail.com>
2023-10-03 11:15:47 -0400

    navbar cleanup & removed ExperienceCard.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,20 +4,8 @@ import '../index.css'
 const NavBar = () => {
   return (
     <nav>
-      {/* <h1 className='navname'>Mohit Pinninti</h1>
-      <ul>
-        <li>Mohit Pinninti</li>
-        <li>Home</li>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Contact</li>
-      </ul> */}
       <h1><Link to={"/"} className='navname'>Mohit Pinninti</Link></h1>
       <ul>
-        {/* <li>Home</li>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Contact</li> */}
         <li><Link to={"/"} className='navlink'>Home</Link></li>
         <li><Link to={"/about"} className='navlink'>About</Link></li>
         <li><Link to={"/career"} className='navlink'>Career</Link></li>
