d0f3c10ef1dd84f20a593e6d9e142c62b59d45a1
Mohit Pinninti <pinnintimohit@gmail.com>
2023-10-11 12:04:15 -0400

    clean branch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import HomeIntroCard from "../components/HomeIntroCard";
 import RecentUpdatesCard from "../components/RecentUpdatesCard";
-import Testing from "../components/Testing";
 
 const HomePage = () => {
   return (
     <>
-      {/* <HomeIntroCard /> */}
-      <Testing />
+      <HomeIntroCard />
       <RecentUpdatesCard />
     </>
   );
