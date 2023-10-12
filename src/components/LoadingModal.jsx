166a7f7b4d0e7e276a397d58327bbbd299b8e07a
Mohit Pinninti <pinnintimohit@gmail.com>
2023-10-12 13:54:48 -0400

    centered modal and made rotated avatar

--- src/components/LoadingModal.jsx
+++ src/components/LoadingModal.jsx
@@ -1,29 +1,11 @@
 import { Html, useProgress } from "@react-three/drei";
 
-// function Loader() {
-//     const { progress } = useProgress();
-//     return <Html center>{progress}%</Html>;
-//   }
-
 const LoadingModal = () => {
-    const { progress } = useProgress();
+  const { progress } = useProgress();
   return (
     <Html>
-    {/* <div className="modal" id="modal-loading" data-backdrop="static">
-      <div className="modal-dialog modal-sm">
-        <div className="modal-content">
-          <div className="modal-body text-center">
-            <div className="loading-spinner mb-2" style={{alignItems: center}}>
-                <h3 className="careerpage-heading-4">{progress}%</h3>
-            </div>
-            // <div>Loading</div>
-          </div>
-        </div>
-      </div>
-    </div> */}
-    <div className="loading-spinner mb-2">
-                <h3 className="careerpage-heading-4">{progress}%</h3>
-            </div>
+      <div className="loading-spinner"></div>
+      <h3 className="modal-progress">{Math.round(progress)}%</h3>
     </Html>
   );
 };
--- src/pages/AvatarCanvas.jsx
+++ src/pages/AvatarCanvas.jsx
@@ -1,10 +1,9 @@
 import {
   OrbitControls,
-  Html,
   useAnimations,
   useGLTF,
   PerspectiveCamera,
-  // Float,
+  Float,
 } from "@react-three/drei";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useEffect, useRef, useState, Suspense } from "react";
@@ -27,7 +26,7 @@ const PlainAvatar = () => {
     Idle: "idle",
   };
 
-  const [animationState, setAnimationState] = new useState(Animations.Greeting);
+  // const [animationState, setAnimationState] = new useState(Animations.Greeting);
 
   // useEffect(() => {
 
@@ -52,68 +51,55 @@ const PlainAvatar = () => {
   //   switchAnimation();
   // }, []);
 
+  const avatarRef = useRef(null);
+
+  let speed = 0.001;
+
+  const rotateDirs = {
+    Left: "left",
+    Right: "right"
+  };
+
+  const [rotate, setRotate] = useState(rotateDirs.Right);
+
+  useFrame(() => {
+    if (!avatarRef) {
+      return;
+    }
+
+    // update rotations
+    if (avatarRef.current.rotation.y < - 0.8 && rotate === rotateDirs.Right) {
+      setRotate(rotateDirs.Left);
+    } else if (avatarRef.current.rotation.y > 0.05 && rotate === rotateDirs.Left) {
+      setRotate(rotateDirs.Right);
+    }
+
+    // update speed
+    if (avatarRef.current.rotation.y < -0.6) {
+      speed = 0.0002;
+    } else if (avatarRef.current.rotation.y > -0.15) {
+      speed = 0.0002;
+    } else {
+      speed = 0.0005;
+    }
+
+    rotate === rotateDirs.Left ? avatarRef.current.rotation.y += speed : avatarRef.current.rotation.y -= speed;
+  });
+
   return (
     <group>
+      {/* <Float floatingRange={[0, 0]} rotationIntensity={10} rotateOnWorldAxis={[0, 0, 1]}> */}
       <primitive
         object={avatar.scene}
         scale={[-3, 3, 3]}
         rotation-y={-Math.PI / 6}
         position={[0, -5, 0]}
+        ref={avatarRef}
       />
+      {/* </Float> */}
     </group>
   );
 };
-
-// const AvatarGroup = () => {
-//   return (
-//     <group position={[0, 0, 0]}>
-//       {/* <ambientLight intensity={0.25} /> */}
-//       <directionalLight
-//         position={[6, 6, -6]}
-//         color={0xffffff}
-//         castShadow
-//         intensity={1}
-//         shadow-mapsize={[1024, 1024]}
-//         shadow-bias={-0.0001}
-//       />
-//       <directionalLight
-//         position={[9, 15, -10]}
-//         color={0x002aff}
-//         castShadow
-//         intensity={20}
-//         shadow-mapsize={[1024, 1024]}
-//         shadow-bias={-0.0001}
-//       />
-
-//       <directionalLight
-//         position={[-9, 15, -10]}
-//         color={0xffffff}
-//         castShadow
-//         intensity={3}
-//       />
-//       <ambientLight position={[0, 0, 0]} color={0xfff5b6} intensity={0.25} />
-//       <spotLight
-//         color={0xfff5b6}
-//         intensity={2}
-//         distance={0}
-//         angle={0.5}
-//         penumbra={0}
-//         decay={1}
-//       />
-//       <PerspectiveCamera
-//         makeDefault
-//         position={[0, 0, 3]}
-//         zoom={1.5}
-//       >
-//         <directionalLight intensity={0.5} position={[2, 0, 0]} />
-//         <directionalLight intensity={0.5} position={[-2, 0, 0]} />
-//       </PerspectiveCamera>
-//       <pointLight position={[2, 0.5, 1]} castShadow intensity={1} />
-//       <spotLight castShadow intensity={50} position={[0, 1, 5]} />
-//       <PlainAvatar />
-//     </group>
-//   );
-// };
 
 function CameraRig() {
   const cameraRef = useRef(null);
